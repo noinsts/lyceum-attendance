178aa389b9e433e24d46468e85c86fa5a1f6ed27
noinsts <shostak303@gmail.com>
2026-05-07 13:05:31 +0300

    feat(admin): add broadcast function in admin panel

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -2,14 +2,22 @@ from datetime import date
 from operator import attrgetter
 
 from aiogram import F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, CallbackQuery, BufferedInputFile
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
 
 from .base import BaseHandler
+from src.utils.keyboards import get_confirmation_keyboard
 from src.db.connector import DBConnector
 from src.utils.keyboards import get_admin_keyboard, get_back_keyboard
 from src.utils.spreedsheet import build_report_excel
+
+
+class BroadcastStates(StatesGroup):
+    waiting_for_message = State()
+    waiting_for_confirmation = State()
 
 
 class AdminHandler(BaseHandler):
@@ -19,8 +27,13 @@ class AdminHandler(BaseHandler):
         self.router.callback_query.register(self.send_report, F.data == 'admin_report')
         self.router.callback_query.register(self.download_report, F.data == 'admin_download_report')
         self.router.callback_query.register(self.did_not_send_report, F.data == 'admin_did_not_send_report')
+        self.router.callback_query.register(self.broadcast_handler, F.data == 'admin_broadcast')
+        self.router.message.register(self.broadcast_recv_message_handler, F.text, BroadcastStates.waiting_for_message)
+        self.router.callback_query.register(self.broadcast_submit_handler, F.data == 'submit', BroadcastStates.waiting_for_confirmation)
+        self.router.callback_query.register(self.handle, F.data == 'cancel', BroadcastStates.waiting_for_confirmation)
 
-    async def handle(self, event: Message | CallbackQuery, db: DBConnector) -> None:
+    async def handle(self, event: Message | CallbackQuery, db: DBConnector, state: FSMContext) -> None:
+        await state.clear()
         name = await db.admins.get_name(event.from_user.id)
         prompt = (
             f"<b>Привіт, {name}</b>\n\n"
@@ -78,11 +91,66 @@ class AdminHandler(BaseHandler):
         sent_reports = await db.reports.get_reports_by_day(date.today())
         did_not_send = [form for form in all_forms if form not in [report.form for report in sent_reports]]
         did_not_send.sort()
+
         prompt = "<b>Список класів, які не надіслали звіт сьогодні:</b>\n\n"
         for form in did_not_send:
             prompt += f"<b>{form}</b>\n"
+
         await callback.message.edit_text(
             prompt,
+            reply_markup=get_back_keyboard('admin'),
+            parse_mode=ParseMode.HTML
+        )
+
+    # ---------------------------------------------------
+    # Broadcast
+    # ---------------------------------------------------
+
+    async def broadcast_handler(self, callback: CallbackQuery, state: FSMContext) -> None:
+        await state.set_state(BroadcastStates.waiting_for_message)
+        prompt = (
+            "📥 <b>Створення оголошення</b>\n\n"
+            "Введіть текст сповіщення, яке надійде всім вчителям"
+        )
+        await callback.message.edit_text(
+            prompt,
+            reply_markup=get_back_keyboard('admin'),
+            parse_mode=ParseMode.HTML
+        )
+
+    async def broadcast_recv_message_handler(self, message: Message, state: FSMContext) -> None:
+        msg = message.text
+        if not msg:
+            return
+        await state.set_state(BroadcastStates.waiting_for_confirmation)
+        await state.update_data(msg=msg)
+        prompt = (
+            f"📥 Ви хочете надіслати повідомлення це повідомленням всім вчителям\n\n"
+            f"<blockquote>{msg}</blockquote>\n\n"
+            f"<i>вірно?</i>"
+        )
+        await message.answer(
+            prompt,
+            reply_markup=get_confirmation_keyboard(),
+            parse_mode=ParseMode.HTML
+        )
+
+    async def broadcast_submit_handler(self, callback: CallbackQuery, state: FSMContext, db: DBConnector) -> None:
+        msg = (await state.get_data()).get('msg', '')
+        if not msg:
+            return
+        users = await db.users.get_all_users()
+        sender = await db.admins.get_name(callback.from_user.id)
+        response = (
+            f"📥 <b>Оголошення</b>\n\n"
+            f"<blockquote>{msg}</blockquote>\n\n"
+            f"<i>Від: {sender}</i>"
+        )
+        for user in users:
+            await callback.bot.send_message(user.user_id, response, parse_mode=ParseMode.HTML)
+        await state.clear()
+        await callback.message.edit_text(
+            "✅ Повідомлення успішно надіслано!",
             reply_markup=get_back_keyboard('admin'),
             parse_mode=ParseMode.HTML
         )
